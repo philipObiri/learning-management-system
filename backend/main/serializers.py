e7fdf2bafadc922d53fd1cf1b1b11cea4274fac0
Philip Obiri <philipobiri3.1@gmail.com>
2023-12-19 14:32:53 +0000

    task: serilaize teacher model

---
+++ backend/main/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Teacher, CourseCategory, Course, Student
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = [
+            "id",
+            "full_name",
+            "email",
+            "password",
+            "qualification",
+            "mobile_number",
+            "address",
+        ]
